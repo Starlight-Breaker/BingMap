8d5ba408c95aae6c7188beb773b741fba72c9cae
StarlightBreaker <alipiojanwesley@gmail.com>
2019-09-06 15:51:18 +0800

    BingMap

---
+++ WesleyAlipio.BingMap.Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace WesleyAlipio.BingMap.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Map_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+
+            Point mousePosition = e.GetPosition(this);
+            Pushpin pin = new Pushpin();
+            pin.Location = MyMap.ViewportPointToLocation(mousePosition);
+            MyMap.Children.Add(pin);
+            var lat = pin.Location.Latitude;
+            var lon = pin.Location.Longitude;
+        }
+    }
+    
+}
+
